1ca1f642ffaef39e2085f69c183fbd52342a6c85
Raghu Betina <raghu@firstdraft.com>
2016-11-17 18:58:15 +0000

    Direct association between users and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :comments,
+             :dependent => :nullify
+
   has_many   :responses,
              :dependent => :nullify
 
