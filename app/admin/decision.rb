1756d5c8a765be64df6cb5076616051432805711
Raghu Betina <raghu@firstdraft.com>
2016-11-17 18:58:44 +0000

    Whitelist columns in Decision for ActiveAdmin

--- app/admin/decision.rb
+++ app/admin/decision.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Decision do
 
+ permit_params :user_id, :name, :description
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
