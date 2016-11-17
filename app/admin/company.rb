9f0ec067a3ac22391db740e9a11ae9cc8295871a
Raghu Betina <raghu@firstdraft.com>
2016-11-17 18:58:48 +0000

    Whitelist columns in Company for ActiveAdmin

--- app/admin/company.rb
+++ app/admin/company.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Company do
 
+ permit_params :company_name, :company_url, :industry
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
