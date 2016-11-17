7c02e0ce96c17c762d522520382d8cd5101ea72e
Raghu Betina <raghu@firstdraft.com>
2016-11-17 18:58:10 +0000

    Validate presence of users.first_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Validations
 
+  validates :first_name, :presence => true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
