34d1115fb6114e0146f3eebe2c7c600e44596d45
Raghu Betina <raghu@firstdraft.com>
2016-11-17 18:59:03 +0000

    Only response user can modify response

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,4 +1,14 @@
 class ResponsesController < ApplicationController
+  before_action :current_user_must_be_response_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_response_user
+    response = Response.find(params[:id])
+
+    unless current_user == response.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @responses = Response.all
 
