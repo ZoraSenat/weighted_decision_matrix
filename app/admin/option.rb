47d013e286c41547f83cfc471395b10328d2833c
Raghu Betina <raghu@firstdraft.com>
2016-11-17 18:59:03 +0000

    Whitelist columns in Option for ActiveAdmin

--- app/admin/option.rb
+++ app/admin/option.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Option do
 
+ permit_params :name, :descriptions, :decision_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
