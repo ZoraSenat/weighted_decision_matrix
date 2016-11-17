7988259472350a7334db9747cb2a55b72dcd739d
Raghu Betina <raghu@firstdraft.com>
2016-11-17 18:58:56 +0000

    Whitelist columns in Criterium for ActiveAdmin

--- app/admin/criterium.rb
+++ app/admin/criterium.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Criterium do
 
+ permit_params :weight, :decision_id, :name, :user_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
