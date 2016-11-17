94bff299a11499dc107e0da437f3440131738a13
Raghu Betina <raghu@firstdraft.com>
2016-11-17 18:59:03 +0000

    Only comment user can modify comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,14 @@
 class CommentsController < ApplicationController
+  before_action :current_user_must_be_comment_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_comment_user
+    comment = Comment.find(params[:id])
+
+    unless current_user == comment.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @comments = Comment.all
 
