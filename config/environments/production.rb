dcbe47a83d5e79f58698d72b0ea2397a59861cbc
zora <ssh>
2016-12-07 19:03:06 -0600

    Removed email to new users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,17 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.delivery_method = :smtp
-# SMTP settings for gmail
-config.action_mailer.smtp_settings = {
- :address              => "zorasenat.gmail.com",
- :port                 => 587,
- :user_name            => ENV['zorasenat'],
- :password             => ENV['221Bbaker!'],
- :authentication       => "plain",
-:enable_starttls_auto => true
-}
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
